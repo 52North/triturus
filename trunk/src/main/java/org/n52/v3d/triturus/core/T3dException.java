b5fd325ef61c2fb0e5088255ef8d82143db04460
BennoSchmidt <benno.schmidt@hs-bochum.de>
2011-06-09 16:37:33 +0000

    added 52N-Headers and engl. documentation

--- trunk/src/main/java/org/n52/v3d/triturus/core/T3dException.java
+++ trunk/src/main/java/org/n52/v3d/triturus/core/T3dException.java
@@ -1,21 +1,41 @@
+/***************************************************************************************
+ * Copyright (C) 2011 by 52 North Initiative for Geospatial Open Source Software GmbH  *
+ *                                                                                     *
+ * Contact: Benno Schmidt & Martin May, 52 North Initiative for Geospatial Open Source *
+ * Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster, Germany, info@52north.org *
+ *                                                                                     *
+ * This program is free software; you can redistribute and/or modify it under the      *
+ * terms of the GNU General Public License version 2 as published by the Free Software *
+ * Foundation.                                                                         *
+ *                                                                                     *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied WARRANTY *
+ * OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public  *
+ * License for more details.                                                           *
+ *                                                                                     *
+ * You should have received a copy of the GNU General Public License along with this   *
+ * program (see gnu-gpl v2.txt). If not, write to the Free Software Foundation, Inc.,  *
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or visit the Free Software *
+ * Foundation web page, http://www.fsf.org.                                            *
+ **************************************************************************************/
 package org.n52.v3d.triturus.core;
 
 /**
- * Rahmenwerk-spezifische Ausnahme.<p>
- * <tt>T3dException</tt> ist eine Spezialisierung der Klassen <tt>RuntimeException</tt> und <tt>Throwable</tt>.<p>
+ * Framework-specific exception.<br /><br />
+ * <i>German:</i> </i>Rahmenwerk-spezifische Ausnahme. <tt>T3dException</tt> ist eine Spezialisierung der Klassen
+ * <tt>RuntimeException</tt> und <tt>Throwable</tt>.
  * @see T3dExceptionMessage
- * @author Benno Schmidt<br>
- * (c) 2003-2005, con terra GmbH & Institute for Geoinformatics<br>
+ * @author Benno Schmidt
  */
 public class T3dException extends RuntimeException
 {
     private int mId = -1;
 
     /**
-     * Konstruktor.<p>
-     * Bem.: Die Verwendung der Fehler-IDs erfolgt in der Klasse <tt>T3dExceptionMessage</tt>.<p> 
-     * @param pMsg Fehlermeldung
-     * @param pErrId eindeutige ID
+     * Constructor.<br />
+     * Note: The error IDs will be used by the class <tt>T3dExceptionMessage</tt>.<br /><br />
+     * <i>German:</i> Bem.: Die Verwendung der Fehler-IDs erfolgt in der Klasse <tt>T3dExceptionMessage</tt>.
+     * @param pMsg Error message
+     * @param pErrId unique ID
      */
     public T3dException(String pMsg, int pErrId) {
         super(pMsg);
@@ -23,8 +43,8 @@ public class T3dException extends RuntimeException
     }
 
 	/**
-	 * Konstruktor.<p>
-	 * @param pMsg Fehlermeldung
+	 * Constructor.<p>
+	 * @param pMsg Error message
 	 */
 	public T3dException(String pMsg) {
 		super(pMsg);
@@ -32,14 +52,15 @@ public class T3dException extends RuntimeException
 
 	/**
      * @deprecated
-     * Konstruktor.
      */
 	public T3dException() {
 		super();
 	}
 
     /**
-     * liefert die ID der <tt>T3dException</tt>. Falls keine ID gesetzt ist, wird der Wert -1 zurückgegeben.<p>
+     * returns the <tt>T3dException</tt>'s ID. If not ID is set, the method will return -1.<br /><br />
+     * <i>German:</i> liefert die ID der <tt>T3dException</tt>. Falls keine ID gesetzt ist, wird der Wert -1
+     * zur&uuml;ckgegeben.
      * @return ID
      */
     public int getId() {
--- trunk/src/main/java/org/n52/v3d/triturus/core/T3dProcRendererMapper.java
+++ trunk/src/main/java/org/n52/v3d/triturus/core/T3dProcRendererMapper.java
@@ -1,27 +1,46 @@
+/***************************************************************************************
+ * Copyright (C) 2011 by 52 North Initiative for Geospatial Open Source Software GmbH  *
+ *                                                                                     *
+ * Contact: Benno Schmidt & Martin May, 52 North Initiative for Geospatial Open Source *
+ * Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster, Germany, info@52north.org *
+ *                                                                                     *
+ * This program is free software; you can redistribute and/or modify it under the      *
+ * terms of the GNU General Public License version 2 as published by the Free Software *
+ * Foundation.                                                                         *
+ *                                                                                     *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied WARRANTY *
+ * OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public  *
+ * License for more details.                                                           *
+ *                                                                                     *
+ * You should have received a copy of the GNU General Public License along with this   *
+ * program (see gnu-gpl v2.txt). If not, write to the Free Software Foundation, Inc.,  *
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or visit the Free Software *
+ * Foundation web page, http://www.fsf.org.                                            *
+ **************************************************************************************/
 package org.n52.v3d.triturus.core;
 
 /**
- * Abstrakte Basisklasse für Renderer-Mapper-Objekte. Renderer-Mapper dienen zur Transformation von abstrakten,
- * Renderer-unabhängigen Visualisierungsobjekten in (z. B. abstraktes Shape -&gt; Java 3D-Shape); vgl. Konzept der
- * Visualisierungs-Pipeline.
- * <p>
- * Die Transformation erfolgen in den Implementierungen über geeignete <tt>transform</tt>-Methoden der Art
- * <tt>public Object transform(Object pInput)</tt>, wobei in <tt>pInput</tt> abstrakte, Renderer-unabhängige
- * Visualisierungsobjekte übergeben werden und das Ergebnisobjekt Renderer-epszifische Visualisierungsobjekte umfasst.
- * <p>
- * Als spezielle Modellierung für abstrakte Visualisierungsobjekte ist z. B. das Paket <tt>org.n52.v3d.triturus.vscene</tt>
- * nutzbar.
- * <p>
- * Hinweis: Realisierungen dieser abstrakten Basisklasse sollten nach Möglichkeit mit dem Präfix "Mpr" versehen werden.
- * <p>
+ * @deprecated
+ * Abstract base class for renderer-mapper objects. <i>Renderer-mapper objects</i> transform abstract,
+ * Renderer-independent visualization objects into Renderer-specific shapes (e.g. abstract visualization object
+ * <tt>-></tt> Java 3D shape).<br />
+ * Note: Realizations of this abstract base class usually are begin with the prefix &quot;Mpr&quot; as class name.
+ * <br /><br />
+ * <i>German:</i> Abstrakte Basisklasse f&uml;r Renderer-Mapper-Objekte. Renderer-Mapper-Objekte dienen zur
+ * Transformation von abstrakten, Renderer-unabh&auml;ngigen Visualisierungsobjekten in Renderer-spezifische Shapes
+ * (z. B. abstracktes Visualisierungsobjekt <tt>-&gt;</tt> Java 3D-Shape).<br /><br />
+ * Hinweis: Realisierungen dieser abstrakten Basisklasse sollten nach M&ouml;glichkeit mit dem Pr&auml;fix
+ * &quot;Mpr&quot; versehen werden.<br />
  * @see T3dProcMapper
  * @see T3dProcFilter
  * @see IoObject
- * @author Benno Schmidt und Torsten Heinen<br>
- * (c) 2004, con terra GmbH & Institute for Geoinformatics<br>
+ * @author Benno Schmidt, Torsten Heinen
  */
 abstract public class T3dProcRendererMapper
 {
-	/** protokolliert die durchgeführte Transformation. */
+    /** protocols the transformation.<br /><br />
+    * <i>German:</i> protokolliert die durchgef&uuml;hrte Transformation.
+    * @return Text to be logged
+    */
 	abstract public String log();
 }
