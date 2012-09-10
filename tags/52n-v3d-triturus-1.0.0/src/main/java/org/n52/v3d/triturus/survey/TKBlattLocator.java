a032f9f0decdddd63953298be49f1f8660289768
BennoSchmidt <benno.schmidt@hs-bochum.de>
2012-09-10 15:34:55 +0000

    [maven-release-plugin]  copy for tag 52n-v3d-triturus-1.0.0

---
+++ tags/52n-v3d-triturus-1.0.0/src/main/java/org/n52/v3d/triturus/survey/TKBlattLocator.java
@@ -0,0 +1,171 @@
+/***************************************************************************************
+ * Copyright (C) 2011 by 52 North Initiative for Geospatial Open Source Software GmbH  *
+ *                                                                                     *
+ * Contact: Benno Schmidt & Martin May, 52 North Initiative for Geospatial Open Source *
+ * Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster, Germany, info@52north.org *
+ *                                                                                     *
+ * This program is free software; you can redistribute and/or modify it under the      *
+ * terms of the GNU General Public License version 2 as published by the Free Software *
+ * Foundation.                                                                         *
+ *                                                                                     *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied WARRANTY *
+ * OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public  *
+ * License for more details.                                                           *
+ *                                                                                     *
+ * You should have received a copy of the GNU General Public License along with this   *
+ * program (see gnu-gpl v2.txt). If not, write to the Free Software Foundation, Inc.,  *
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or visit the Free Software *
+ * Foundation web page, http://www.fsf.org.                                            *
+ **************************************************************************************/
+package org.n52.v3d.triturus.survey;
+
+import java.util.HashMap;
+import org.n52.v3d.triturus.vgis.VgPoint;
+import org.n52.v3d.triturus.vgis.T3dSRSException;
+import org.n52.v3d.triturus.core.T3dException;
+
+/**
+ * Determination of German TK-Blattnummer identifers.<br /><br />
+ * <i>German:</i> Klasse zur Ermittlung der Blattnummern der Topografischen Karten TK 25, TK 50, TK 100 der
+ * bundesdeutschen Landesvermessungs&auml;mter.<br />
+ * Bem.: Dem Blattschnitt der TK 25 liegt dabei eine Gradabteilungskarte mit Linien geografischer Koordinaten in
+ * Abst�nden von 10 L�ngenminuten (10' entsprechend 1/6 Grad) und 6 Breitenminuten (6' entsprechend 1/10 Grad)
+ * zugrunde.
+ * @author Benno Schmidt
+ */
+public class TKBlattLocator
+{
+	private HashMap mBlattnamen;
+	
+	/**
+     * Constructor.
+     */
+	public TKBlattLocator() {
+		this.initBlattnamen();
+	}
+	 
+	/**
+     * returns the TK-identifier for a given point.<br /><br />
+	 * <i>German:</i> liefert die Nummer des TK-Blattes f&uuml;r den angegebenen Punkt.<br />
+	 * @param pTKBez TK-Werk, z. B. <tt>"TK 25"</tt>, <tt>"TK 50"</tt> oder <tt>"TK 100"</tt>
+	 * @param pt Position gegeben in geografischen Koordinaten
+	 * @return Blattnummer
+	 * @throws org.n52.v3d.triturus.core.T3dException
+	 * @throws org.n52.v3d.triturus.vgis.T3dSRSException
+	 */
+    // todo engl. javadoc der Parameter
+    public String blattnummer(String pTKBez, VgPoint pt) throws T3dException, T3dSRSException
+	{
+  		if (! (pt.getSRS().equalsIgnoreCase("EPSG:4326")))
+            throw new T3dSRSException( "TKBlattLocator can not process SRS \"" + pt.getSRS() + "\"." );
+
+		if (pTKBez.equalsIgnoreCase("TK 25") || pTKBez.equalsIgnoreCase("TK25"))
+			return this.tk25(pt);
+		if (pTKBez.equalsIgnoreCase("TK 50") || pTKBez.equalsIgnoreCase("TK50"))
+			return this.tk50(pt);
+		if (pTKBez.equalsIgnoreCase("TK 100") || pTKBez.equalsIgnoreCase("TK100"))
+			return this.tk100(pt);
+		// else:
+		throw new T3dException( "Illegal TK name: \"" + pTKBez + "\"." );
+	}
+
+    /**
+     * @see TileLocator#tileNumber(String, org.n52.v3d.triturus.vgis.VgPoint)
+     */
+    public String tileNumber(String pTileId, VgPoint pt) throws T3dException, T3dSRSException {
+        return this.blattnummer(pTileId, pt);
+    }
+
+    /**
+     * formats the TK-Blattnummer.<br /><br />
+     * <i>German:</i> formatiert die TK-Blattnummer f&uuml;r die angegebene Nummern-Kombination.<br />
+     * Beispiel: <tt>blattnummer(47,9)</tt> liefert &quot;4709&quot; als Resultat.
+     * @param i ersten beiden Ziffern der Blattnummer-Angabe als Ganzzahl
+     * @param j letzten beiden Ziffern der Blattnummer-Angabe als Ganzzahl
+     * @return Blattnummer
+     */
+    // todo engl. javadoc der Parameter
+    static public String blattnummer(int i, int j) {
+        String ret = "";
+        if (i < 10) ret += "0";
+        ret += "" + i;
+        if (j < 10) ret += "0";
+        ret += "" + j;
+        return ret;
+    }
+
+	private int mB12, mB34;
+	
+	private void setTk25(VgPoint pt) 
+	{
+		mB34 = (int)(6. * pt.getX() - 34.);
+		mB12 = (int)(560. - 10. * pt.getY());
+	}
+
+	private String generateNumber() 
+	{
+		if (mB34 < 10)
+			return "" + mB12 + "0" + mB34;
+		else
+			return "" + mB12 + mB34;
+	}
+
+	private String tk25(VgPoint pt) 
+	{
+		this.setTk25(pt);
+		return this.generateNumber();
+	}
+	
+	private String tk50(VgPoint pt) 
+	{
+		this.setTk25(pt);
+		if (mB34 % 2 == 1) mB34 -= 1;
+		if (mB12 % 2 == 0) mB12 += 1;
+		return "L" + this.generateNumber();
+	}
+
+	private String tk100(VgPoint pt) 
+	{
+		this.setTk25(pt);
+		if (mB34 % 4 != 2) mB34 -= ((mB34 + 2) % 4);
+		if (mB12 % 4 != 3) mB12 += (3 - (mB12 % 4));
+		return "C" + this.generateNumber();
+	}
+	
+	private void initBlattnamen() 
+	{
+		mBlattnamen = new HashMap();
+        
+		mBlattnamen.put("4210", "L�dinghausen");
+		mBlattnamen.put("4211", "Ascheberg");
+		mBlattnamen.put("4212", "Drensteinfurt");
+		mBlattnamen.put("4310", "Datteln");
+		mBlattnamen.put("4311", "L�nen");
+		mBlattnamen.put("4312", "Hamm");
+		mBlattnamen.put("4410", "Dortmund");
+		mBlattnamen.put("4411", "Kamen");
+		mBlattnamen.put("4412", "Unna");
+		mBlattnamen.put("L4111", "M�nster");
+		mBlattnamen.put("L4310", "L�nen");
+		mBlattnamen.put("C4310", "M�nster");
+		mBlattnamen.put("C4710", "Dortmund");
+        // Liste kann bei Bedarf erweitert werden...
+	}
+
+	/**
+     * returns the TK-Blatt name for a given TK-Blatt identifiert.<br /><br />
+	 * <i>German:</i> liefert den Namen des TK-Blatts mit der angegebenen Blattnummer.<br />
+	 * Bem.: Diese Methode ist nur f&uuml;r Teilgebiete Nordrhein-Westfalens realisiert; siehe private Methode
+     * TkBlattLocator#initBlattnamen im Quellcoorg.n52.v3d.
+	 * @param pBlattnummer Blattnummer, z. B. aus <tt>this.blattnummer()</tt>.
+	 * @return Blattname oder Leerstring, falls nicht vorliegend.
+	 */
+    // todo engl. javadoc der Parameter
+	public String blattname(String pBlattnummer) 
+	{
+		String ret = (String)(mBlattnamen.get(pBlattnummer));
+		if (ret == null) 
+			return "";
+		return ret;
+	}
+}
---
+++ tags/52n-v3d-triturus-1.0.0/src/main/java/org/n52/v3d/triturus/survey/Utm32Transformator.java
@@ -0,0 +1,179 @@
+/***************************************************************************************
+ * Copyright (C) 2011 by 52 North Initiative for Geospatial Open Source Software GmbH  *
+ *                                                                                     *
+ * Contact: Benno Schmidt & Martin May, 52 North Initiative for Geospatial Open Source *
+ * Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster, Germany, info@52north.org *
+ *                                                                                     *
+ * This program is free software; you can redistribute and/or modify it under the      *
+ * terms of the GNU General Public License version 2 as published by the Free Software *
+ * Foundation.                                                                         *
+ *                                                                                     *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied WARRANTY *
+ * OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public  *
+ * License for more details.                                                           *
+ *                                                                                     *
+ * You should have received a copy of the GNU General Public License along with this   *
+ * program (see gnu-gpl v2.txt). If not, write to the Free Software Foundation, Inc.,  *
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or visit the Free Software *
+ * Foundation web page, http://www.fsf.org.                                            *
+ **************************************************************************************/
+package org.n52.v3d.triturus.survey;
+
+import org.n52.v3d.triturus.vgis.VgPoint;
+import org.n52.v3d.triturus.vgis.T3dSRSException;
+import org.n52.v3d.triturus.vgis.VgGeomObject;
+import org.n52.v3d.triturus.core.T3dException;
+import org.n52.v3d.triturus.survey.coordinatetransform1.CoordinateTransformFactory;
+import org.n52.v3d.triturus.survey.coordinatetransform1.CoordinateTransform;
+import org.n52.v3d.triturus.survey.coordinatetransform1.GeographicTransformException;
+
+import java.lang.Double;
+
+/**
+ * Transformation of positions given in UTM 32.<br /><br />
+ * <i>German:</i> Klasse zur Transformation von Punkten in das/aus dem UTM32-System.<br />
+ * Geografische Koordinaten sind stets dezimal in Altgrad anzugeben. Rechts- und Hochwerte sind in m anzugeben (d. h.,
+ * unter Verwendung von 7 Vorkommastellen).
+ * @author Benno Schmidt
+ */
+public class Utm32Transformator
+{
+	private double mRechts = 0.;
+	private double mHoch = 0.;
+	private double mLaenge = 0.;
+	private double mBreite = 0.;
+
+	/**
+     * transforms coordinates referring to the WGS-84-ellipsoid to UTM32.<br /><br />
+	 * <i>German:</i> transformiert auf den WGS-84-Ellipsoid bezogene geografische Koordinaten in das UTM32-System.
+     * <br />
+	 * Vor dem Methodenaufruf muss das Ergebnisobjekt bereits instanziiert sein (z. B. als <tt>GmPoint</tt>).
+	 * @see VgPoint
+	 * @param pIn zu transformierender Punkt in geografischen Koordinaten (EPSG:4326)
+	 * @param pOut Punkt mit berechneten Koordinaten im UTM32-System
+	 * @throws T3dSRSException
+	 * @throws java.lang.NullPointerException falls <tt>pOut</tt> nicht intanziiert
+	 */
+    // todo engl. javadoc der Parameter
+	public void latLon2Utm(VgPoint pIn, VgPoint pOut) throws T3dSRSException
+	{
+        if (! pIn.getSRS().equalsIgnoreCase(VgGeomObject.SRSLatLonWgs84))
+            throw new T3dSRSException("Tried to process illegal point coordinate.");
+
+        this.geoWgs84InUtm(pIn.getY() /*lat*/, pIn.getX() /*lon*/);
+
+		pOut.setX(mRechts);
+		pOut.setY(mHoch);
+		pOut.setZ(pIn.getZ());
+
+		pOut.setSRS(VgGeomObject.SRSUtmZ32N);
+	}
+
+    /**
+     * transforms coordinates referring to the WGS-84-ellipsoid to UTM32.<br /><br />
+	 * <i>German:</i> transformiert eine auf den WGS-84-Ellipsoid bezogene geografische Koordinate in das UTM32-System.
+     * <br />
+     * @param pLat geografische Breite des zu transformierenden Punktes
+     * @param pLon geografische L&auml;nge des zu transformierenden Punktes
+     * @param pRechts berechneter Rechtswert (UTM32-System)
+     * @param pHoch berechneter Hochwert (UTM32-System)
+     */
+    // todo engl. javadoc der Parameter
+    public void latLon2Gkk(double pLat, double pLon, Double pRechts, Double pHoch)
+    {
+        this.geoWgs84InUtm(pLat, pLon);
+
+        pRechts = new Double(mRechts);
+        pHoch = new Double(mHoch);
+    }
+
+    /**
+     * transforms UTM32 coordinates to geographic coordinates referring to the WGS-84-ellipsoid.<br /><br />
+	 * <i>German:</i> transformiert eine UTM32-Koordinate in auf den WGS-84-Ellipsoid bezogene geografische Koordinaten.
+     * <br />
+     * Vor dem Methodenaufruf muss das Ergebnisobjekt bereits instanziiert sein (z. B. als <tt>GmPoint</tt>).
+     * @param pIn zu transformierender Punkt im UTM32-System
+     * @param pOut Punkt mit berechneten geografischen Koordinaten
+     * @throws java.lang.NullPointerException falls <tt>pOut</tt> nicht intanziiert
+     */
+    // todo engl. javadoc der Parameter
+    public void utm2LatLon(VgPoint pIn, VgPoint pOut)
+    {
+        String srs = pIn.getSRS();
+        if (! srs.equalsIgnoreCase(VgGeomObject.SRSUtmZ32N))
+            throw new T3dSRSException("Tried to process illegal point coordinate (" + srs + ").");
+
+        this.utmInGeoWgs84(pIn.getX(), pIn.getY());
+
+        pOut.setX(mLaenge);
+        pOut.setY(mBreite);
+        pOut.setZ(pIn.getZ());
+
+        pOut.setSRS(VgGeomObject.SRSLatLonWgs84);
+    }
+
+    /**
+     * transforms UTM32 coordinates to geographic coordinates referring to the WGS-84-ellipsoid.<br /><br />
+	 * <i>German:</i> transformiert eine UTM32-Koordinate in auf den WGS-84-Ellipsoid bezogene geografische Koordinaten.
+     * <br />
+     * @param pRechts zu transformierender Rechtswert (UTM32-System)
+     * @param pHoch zu transformierender Hochwert (UTM32-System)
+     * @param pLat berechnete geografische L�nge
+     * @param pLon berechnete geografische Breite
+     */
+     // todo engl. javadoc der Parameter
+    public void utm2LatLon(double pRechts, double pHoch, Double pLat, Double pLon) throws T3dSRSException
+    {
+        this.utmInGeoWgs84(pRechts, pHoch);
+
+        pLat = new Double(mBreite);
+        pLon = new Double(mLaenge);
+    }
+
+	// private Helfer:
+
+    private void geoWgs84InUtm(double breite, double laenge)
+    {
+        CoordinateTransformFactory f = CoordinateTransformFactory.getDefault();
+        CoordinateTransform t = null;
+        double[] src = new double[2];
+        double[] dest = new double[2];
+        src[0] = breite; src[1] = laenge;
+
+        try {
+            t = f.createCoordinateTransform(VgGeomObject.SRSLatLonWgs84, /*VgGeomObject.SRSUtmZ32N -> TODO! */ "EPSG:32632");
+            dest = t.transformCoord(src, dest);
+        }
+        catch (GeographicTransformException e) {
+            throw new T3dException("Coordinate transformation failed: " + e.getMessage());
+        }
+
+        mRechts = dest[0];
+        if (mRechts >= 1.e7 /*32000000.*/)
+            mRechts -= mRechts;
+        mHoch = dest[1];
+    }
+
+    private void utmInGeoWgs84(double rechts, double hoch)
+    {
+        CoordinateTransformFactory f = CoordinateTransformFactory.getDefault();
+        CoordinateTransform t = null;
+        double[] src = new double[2];
+        double[] dest = new double[2];
+        src[0] = rechts;
+        if (src[0] < 1.e6 /*32000000.*/)
+            src[0] += 32000000.;
+        src[1] = hoch;
+
+        try {
+            t = f.createCoordinateTransform(/*VgGeomObject.SRSUtmZ32N -> TODO! */ "EPSG:32632", VgGeomObject.SRSLatLonWgs84);
+            dest = t.transformCoord(src, dest);
+        }
+        catch (GeographicTransformException e) {
+            throw new T3dException("GeographicTransformException: " + e.getMessage());
+        }
+
+        mBreite = dest[1];
+        mLaenge = dest[0];
+    }
+}
