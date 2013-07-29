c2b4ae2255ebf6bb80f69f9f91577621d2bb0d43
BennoSchmidt <benno.schmidt@hs-bochum.de>
2013-07-29 11:42:24 +0000

    (minor changes in documentation)

--- src/main/java/org/n52/v3d/triturus/t3dutil/MpHypsometricColor.java
+++ src/main/java/org/n52/v3d/triturus/t3dutil/MpHypsometricColor.java
@@ -1,17 +1,27 @@
+/***************************************************************************************
+ * Copyright (C) 2012 by 52 North Initiative for Geospatial Open Source Software GmbH  *
+ *                                                                                     *
+ * Contact: Benno Schmidt & Martin May, 52 North Initiative for Geospatial Open Source *
+ * Software GmbH, Martin-Luther-King-Weg 24, 48155 Muenster, Germany, info@52north.org *
+ *                                                                                     *
+ * This program is free software; you can redistribute and/or modify it under the      *
+ * terms of the GNU General Public License version 2 as published by the Free Software *
+ * Foundation.                                                                         *
+ *                                                                                     *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied WARRANTY *
+ * OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public  *
+ * License for more details.                                                           *
+ *                                                                                     *
+ * You should have received a copy of the GNU General Public License along with this   *
+ * program (see gnu-gpl v2.txt). If not, write to the Free Software Foundation, Inc.,  *
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or visit the Free Software *
+ * Foundation web page, http://www.fsf.org.                                            *
+ **************************************************************************************/
 package org.n52.v3d.triturus.t3dutil;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StreamTokenizer;
-import java.util.ArrayList;
-
-import org.n52.v3d.triturus.core.T3dException;
-
 /**
- * Abstrakte Basisklasse zur Ermittlung hypsometrischer Farbwerte.<p>
+ * Abstract base class to derive hypsometric color-values.
+ *
  * @see org.n52.v3d.triturus.t3dutil.MpSimpleHypsometricColor
  * @see org.n52.v3d.triturus.t3dutil.MpGMTHypsometricColor
  * @author Benno Schmidt
@@ -19,9 +29,10 @@ import org.n52.v3d.triturus.core.T3dException;
 abstract public class MpHypsometricColor extends MpQuantitativeValue2Color
 {
     /**
-     * liefert die einem H�henwert zugeordnete Farbe.<p>
-     * @param pElevation H�henwert
-     * @return der H�he zugewiesener Farbwert
+     * provides the color that is assigned to an elevation-value.
+     *
+     * @param pElevation elevation-value
+     * @return Color assigned to the given elevation
      */
     abstract public T3dColor transform(double pElevation);
 }
