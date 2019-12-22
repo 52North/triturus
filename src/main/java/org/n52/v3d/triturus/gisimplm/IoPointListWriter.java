5c2089e57f09b3fe38868dfef647cb9663a097b2
BennoSchmidt <benno.schmidt@hs-bochum.de>
2019-12-22 20:51:43 +0100

    add CSV output for 3D point lists

--- src/main/java/org/n52/v3d/triturus/gisimplm/IoPointListWriter.java
+++ src/main/java/org/n52/v3d/triturus/gisimplm/IoPointListWriter.java
@@ -41,40 +41,46 @@ import java.io.*;
 import java.util.List;
 
 /**
- * Export of files that contain point coordinates. Here, a special simple ASCII 
- * format us used: Each line of the input files gives x-, y- and z-coordinate
- * separated by a space character (&quot; &quot;).
+ * Export of files that contain point coordinates. 
  * 
  * @author Benno Schmidt
  */
 public class IoPointListWriter extends IoObject
 {
-    private String mLogString = "";
-    private String mFormat;
+    private String logString = "";
+    private String format;
 
     
     /**
      * File-format type identifier to be used for plain ASCII files holding 
-     * XYZ triples.
+     * x-y-z triples. Each line of the target file gives x-, y- and 
+     * z-coordinate separated by a space character (&quot; &quot;).
      */
     public static final String PLAIN = "Plain";
+    /**
+     * File-format type identifier to be used for plain ASCII files holding 
+     * x-y-z triples. Each line of the target file gives x-, y- and 
+     * z-coordinate separated by a comma character (&quot;,&quot;).
+     */
+    public static final String CSV = "CSV";
 
     /**
      * Constructor. As an input parameter, the file format type identifier must
      * be specified. The supported formats are listed below:<br />
      * <ul>
      * <li><i>Plain:</i> ASCII file, x, y and z line by line, separated by space character</li>
+     * <li><i>CSV:</i> ASCII file, x, y and z line by line, separated by comma character</li>
      * </ul>
      * @param pFormat Format-string, e.g. "Plain"
      * @see IoPointListWriter#PLAIN
      */
-    public IoPointListWriter(String pFormat) {
-        mLogString = this.getClass().getName();
-        this.setFormatType(pFormat);
+    public IoPointListWriter(String format) {
+        logString = this.getClass().getName();
+        this.setFormatType(format);
     }
 
     public String log() {
-        return mLogString;
+        return logString;
     }
 
     /** 
@@ -83,32 +89,38 @@ public class IoPointListWriter extends IoObject
      * @param pFormat Format-type (e.g. <tt>&quot;Plain&quot;</tt>)
      * @see IoPointListWriter#PLAIN
      */
-    public void setFormatType(String pFormat)
+    public void setFormatType(String format)
     {
-        mFormat = pFormat;
+        this.format = format;
     }
 
     /**
      * writes a set of 3-d points to a file. 
      * 
      * @param {@link List} consisting of {@link VgPoint} objects to be written
-     * @param pFilename File name (with path optionally)
+     * @param filename File name (with path optionally)
      * @throws org.n52.v3d.triturus.core.T3dException
      * @throws org.n52.v3d.triturus.core.T3dNotYetImplException
      */
-    public void writeToFile(List<VgPoint> pPoints, String pFilename) 
+    public void writeToFile(List<VgPoint> points, String filename) 
     	throws T3dException
     {
         int i = 0;
-        if (mFormat.equalsIgnoreCase("Plain")) {
+        if (format.equalsIgnoreCase("Plain")) {
         	i = 1;
+        }
+        if (format.equalsIgnoreCase("CSV")) {
+        	i = 2;
         }
         // --> add more types here...
 
         try {
             switch (i) {
                 case 1: 
-                	this.writePlainAscii(pPoints, pFilename); 
+                	this.writePlainAscii(points, filename, false); 
+                	break;
+                case 2: 
+                	this.writePlainAscii(points, filename, true); 
                 	break;
                 // --> add more types here...
 
@@ -121,21 +133,23 @@ public class IoPointListWriter extends IoObject
         }
     }
 
-    private void writePlainAscii(List<VgPoint> pPoints, String pFilename) 
+    private void writePlainAscii(
+    	List<VgPoint> points, String filename, boolean commaSeparated) 
     	throws T3dException
     {
     	// TODO: Keep configurable: Separator, x-y-z order, number of written digits,
     	// bounding-box filter, skip point-identifiers etc.
         try {
-            BufferedWriter lDat = new BufferedWriter(new FileWriter(pFilename));
-            for (VgPoint p : pPoints) {
-                lDat.write(p.getX() + " " + p.getY() + " " + p.getZ());
+            BufferedWriter lDat = new BufferedWriter(new FileWriter(filename));
+            String sep = commaSeparated ? "," : " ";
+            for (VgPoint p : points) {
+                lDat.write(p.getX() + sep + p.getY() + sep + p.getZ());
                 lDat.newLine();           	
             }
             lDat.close();
         }
         catch (FileNotFoundException e) {
-            throw new T3dException("Could not access file \"" + pFilename + "\".");
+            throw new T3dException("Could not access file \"" + filename + "\".");
         }
         catch (Throwable e) {
             throw new T3dException(e.getMessage());
